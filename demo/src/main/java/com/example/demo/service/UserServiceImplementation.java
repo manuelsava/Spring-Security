1ca1630a01f41f8f92d88f0a79f7d97d1cfc19b7
manuelsava <manuelsava98@gmail.com>
2022-07-19 12:10:37 +0200

    Security Config

--- demo/src/main/java/com/example/demo/service/UserServiceImplementation.java
+++ demo/src/main/java/com/example/demo/service/UserServiceImplementation.java
@@ -6,18 +6,48 @@ import com.example.demo.repo.RoleRepo;
 import com.example.demo.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImplementation implements UserService{
+public class UserServiceImplementation implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null){
+            log.error("User not found!");
+            throw new UsernameNotFoundException("User not found!");
+        } else {
+            log.info("User found {}", username);
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(
+                role -> {authorities.add(
+                        new SimpleGrantedAuthority(role.getName())
+                );}
+        );
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
+    }
 
     @Override
     public User saveUser(User user) {
